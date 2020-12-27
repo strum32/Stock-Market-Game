aa4e47f057192d5159e7a29928355a1102b21ea1
Steven Strumolo <stevenstrumolo@Stevens-MacBook-Air.local>
2020-12-27 15:05:36 -0500

    added different endpoints

--- app.js
+++ app.js
@@ -1,57 +1,46 @@
-
-
-//const BASE_URL = "https://cloud.iexapis.com/stable/stock/`${symbol}`/company/2?token=pk_0d683e6002274503af0499604ebead92&period=annual"
 const ticker = document.querySelector("#ticker")
 const tickerBtn = document.querySelector("#tickerBtn")
 const url = 'https://www.alphavantage.co/query?function=OVERVIEW&symbol='
 const apiKey = '&apikey=demoYVLLN54HCHSO7WZX'
 const dataDiv = document.querySelector(".data")
 const main1 = document.querySelector(".main")
+const inputValue = document.querySelector('input').value
 
 
 const getData = async () => {
-  // const url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${input.value}&apikey=demoYVLLN54HCHSO7WZX`
-  const inputValue = document.querySelector('input').value
+  const url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${ticker.value}&apikey=demoYVLLN54HCHSO7WZX`
+  const url2 = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${ticker.value}interval=5min&apikey=demoYVLLN54HCHSO7WZX`
+  const url3 = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${ticker.value}&apikey=demoYVLLN54HCHSO7WZX`
   try {
-    const response = await axios.get(`${url}${inputValue}${apiKey}`)
-    const stockData = response.data
-    console.log(stockData)   
+    console.log(ticker.value)
+    const response = await axios.get(`${url}`)
+    const response2 = await axios.get(`${url2}`)
+    const response3 = await axios.get(`${url3}`)
+    const stockData = response.data.bestMatches
+    const stockInfo = response2
+    const companyData = response3.data
+    console.log(stockInfo)
+    console.log(companyData)
+    console.log(stockData[0]['2. name'])   
     
-    dataDiv.innerHTML += `
-    <h1>${stockData.Name}</h1>
-    <div class="currentPrice">
-    <h1>${stockData.BookValue}</h1>
-    </div>
+    let dataInfo = `
+    <h1>${stockData[0]["2. name"]}</h1>
     <div class="analystPrice">
-     <h1>${stockData.AnalystTargetPrice}</h1>
+     <h3>Analyst Price ${companyData.AnalystTargetPrice}</h3>
     </div>
     <div class="stats">
-    <h1>52 Week High:${stockData.WeekHigh}</h1>
-    <h1>${stockData.AnalystTargetPrice}</h1>
-    <h1>${stockData.AnalystTargetPrice}</h1>
-    <h1>${stockData.AnalystTargetPrice}</h1>
+    <h3>Dividend Yield ${companyData.DividendYield}</h3>
     </div>
       `
+    dataDiv.innerHTML = dataInfo
 
-    
-    
-    
-    inputValue = ""
   } catch (error) {
     console.log(error)
   }
 }
 
 
-
-
-
-
-
 tickerBtn.addEventListener("click", getData);
-
-
-
 
 
 
