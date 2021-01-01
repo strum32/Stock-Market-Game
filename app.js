6804f600426ec219031c7dee8a7c938295edd2f7
Steven Strumolo <stevenstrumolo@Stevens-Air.home>
2021-01-01 18:16:23 -0500

    market page set up

--- app.js
+++ app.js
@@ -31,33 +31,39 @@ const getData = async () => {
     <div class="symbol">
     <h1>${companyData.Symbol}</h1>
     </div>
+
     <div class="price">
-    <h2>$ ${stockInfo[stockInfoKey]["4. close"]}</h2>
+    <h2>$ ${stockInfo[stockInfoKey]["1. open"]}</h2>
     </div>
+
+    <div class="analyst">
+    <h2>Analyst Target Price: ${companyData.AnalystTargetPrice}</h2>
+    </div>
+
     <div class="combine">
     <h3>${stockData[0]["2. name"]}</h3>
     <h3> ${companyData.Exchange}
     <h3> ${companyData.Country}</h3>
     </div>
-   </div>
+  
     <div class="stats">
-    <h3>Close : ${stockInfo[stockInfoKey]["4. close"]}</h3>
+    <h3>Open: ${stockInfo[stockInfoKey]["1. open"]}</h3>
     <h3>High: ${stockInfo[stockInfoKey]["2. high"]}</h3>
     <h3>Volume: ${stockInfo[stockInfoKey]["5. volume"]}</h3>
     <h3>52 Week High: ${companyData['52WeekHigh']}</h3>
     <h3>Market Cap: ${companyData.MarketCapitalization}</h3>
     </div>
 
-    <div class="stats2"
+    <div class="stats2">
     <h3>52 Week Low: ${companyData['52WeekLow']}</h3>
     <h3>Low: ${stockInfo[stockInfoKey]["3. low"]}</h3>
     <h3>Dividend Yield: ${companyData.DividendYield}</h3>
     <h3>Close : ${stockInfo[stockInfoKey]["4. close"]}</h3>
     <h3>Earnings Per Share: ${companyData.EPS}</h3>
     </div>
-
+    
     <div class="description">
-    <p> Company Description ${companyData.Description}</p>
+    <p>${companyData.Description}</p>
     </div>
       `
     dataDiv.innerHTML = dataInfo
@@ -66,7 +72,6 @@ const getData = async () => {
     console.log(error)
   }
 }
-
 
 tickerBtn.addEventListener("click", getData);
 
